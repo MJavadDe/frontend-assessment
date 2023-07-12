4b41bfa1a0439af395168717843557cfff3e690e
M, Javad Delavari <mjdelaavaarii@gmail.com>
2023-07-12 16:12:04 +0330

    feat: Doctor Head card created , share window function implanted, view count boosted with js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import { useEffect, useState } from "react";
 import Service from "./components/service";
 import logo from "./logo.svg";
+import { SaveIcon } from "./components/icons/save";
+import { ShareIcon } from "./components/icons/share";
+import {ViewIcon} from './components/icons/view'
+import { Avatar } from "./components/Avavtar";
+import { LikeIcon } from "./components/icons/like";
+import { TrustIcon } from "./components/icons/trust";
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [saveFill, setSaveFill] = useState(false)
+
+  const changeSaveFill = () => setSaveFill(prev => !prev)
+
+  const numberFormatter = (num) =>{
+    return num >= 1000 ? (num / 1000).toFixed() + "K" : num
+  }
+
+  const shareWindow = async () =>{
+    let pageUrl = {
+      url:window.location.href
+    }
+    await navigator.share(pageUrl)
+  }
 
   useEffect(() => {
     fetch("/profile.json")
@@ -20,6 +40,7 @@ const App = () => {
     //   satisfaction,
     //   commentsCount,
     //   waitingTime
+    document.title = data?.name +" "+ data?.family
   }, []);
 
   if (!data) return null;
@@ -41,13 +62,39 @@ const App = () => {
       </header>
       <main className="container grid grid-cols-3 pt-10 mx-auto gap-7">
         <div className="flex flex-col col-span-2 gap-5">
-          {/* Doctor Head */}
-          <div className="bg-white rounded-lg h-96 bg-opacity-60" />
-          <div className="h-56 bg-white rounded-lg bg-opacity-60" />
+          <div className="h-56 bg-white rounded-lg bg-opacity-60 flex flex-col p-4 gap-4">
+            <div className="flex justify-between">
+              <div className="flex gap-3">
+                <div className="flex items-center cursor-pointer gap-1" onClick={changeSaveFill}><SaveIcon fill={saveFill}/> <span>Save</span></div>
+                <div className="flex items-center cursor-pointer gap-1" onClick={shareWindow}><ShareIcon/> <span>Share</span></div>
+              </div>
+              <div className="flex items-center"><ViewIcon/> <span>{numberFormatter(data?.viewCount)}</span></div>
+            </div>
+            <div className="bg-slate-100 flex items-center gap-3 rounded-md px-2">
+              <div>
+                <Avatar src={data?.image}/>
+              </div>
+              <div>
+                <h3 className="font-bold flex items-center gap-2">{data?.name + " " + data?.family} <span className={data?.satisfaction >= 90 && data?.commentsCount >= 100 && data?.waitingTime == 1 ? "inline" : "hidden"}> <TrustIcon/></span></h3>
+                <p>{data?.expertise}</p>
+              </div>
+            </div>
+            <div>
+              <p className="flex items-center gap-2 justify-center">
+                <span className="flex items-center py-1 px-4 bg-green-700 text-white rounded-3xl gap-1">
+                  <LikeIcon/> {data?.satisfaction + "%"}
+                </span>
+                Satisfaction ({data?.commentsCount +" " + "Comments"})
+              </p>
+            </div>
+          </div>
+          <div className="bg-white rounded-lg h-96 bg-opacity-60">
+          </div>
         </div>
         <div className="flex flex-col gap-5">
-          <div className="bg-white rounded-lg bg-opacity-60 h-52" />
-          <Service waitingTime={data?.waitingTime} />
+          <div className="bg-white rounded-lg bg-opacity-60 h-52">
+          </div>
+            <Service waitingTime={data?.waitingTime} />
         </div>
       </main>
     </div>
